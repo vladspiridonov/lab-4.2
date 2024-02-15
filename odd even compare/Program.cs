074a6513f61f37b2d266572199cdef9bc5c5b44a
vladspiridonov <vlad777.96@mail.ru>
2024-02-16 00:40:36 +0300

    Добавьте файлы проекта.

---
+++ odd even compare/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odd_even_compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int Pos = 0;
+            int Neg = 0;
+            do
+            {
+                i = Convert.ToInt32(Console.ReadLine());
+                if (i > 0)
+                {
+                    Pos++;
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        break;
+                    }
+                    Neg++;
+                }
+            } while (i != 0);
+            int Diff = Pos - Neg;
+            if ((Pos == 0) && (Neg == 0))
+            {
+                Console.WriteLine("Числа не введены");
+            }
+            else
+            {
+                if (Diff > 0)
+                {
+                    Console.WriteLine("Больше положительных");
+                }
+                else
+                {
+                    if (Diff == 0)
+                    {
+                        Console.WriteLine("Кол-во одинаково");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Больше отрицательных");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
